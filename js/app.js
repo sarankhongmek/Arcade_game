69bf1f211d188efc440eaff647856b764529bd08
Saran-Khongmek <James.warrender1@btinternet.com>
2018-08-19 15:57:06 +0100

    first init

--- js/app.js
+++ js/app.js
@@ -24,11 +24,20 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class player  {
+
+}
+
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+let allEnemies = [];
 // Place the player object in a variable called player
+
+let newplayer = new player();
+
 
 
 
