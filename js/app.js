c1cc6aec283fde875cbdd4ee8f4d30200e79a78a
Saran-Khongmek <James.warrender1@btinternet.com>
2018-08-24 22:32:44 +0100

    game beta

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +17,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
+    if (this.x > 505)
+    {
+      this.x = -101;
+      this.speed =  Math.floor((Math.random() * 410) + 250);
+
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,24 +34,62 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
 
+  constructor (x, y,) {
+    this.x = 202;
+    this.y = 390;
+    this.sprite = 'images/char-boy.png';
+  }
+
+  update(){
+    for (let enemy of allEnemies){
+      if (this.y === enemy['y'] && (enemy['x'] + 50 > this.x && enemy['x'] < this.x + 50)){
+        this.x = 202;
+        this.y = 390;
+      } else if (this.y < 0){
+        this.x = 202;
+        this.y = 390;
+        swal({  type: "success",
+                title:'Congratulations! You Won!',
+                confirmButtonText:'Play again!',
+
+              })
+      }
+    }
+
+  }
+
+  render(){
+    ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
+  }
+//handle method of the Player
+//condition to check the player is w
+  handleInput(keyPress){
+    if(keyPress === 'up' && this.y > -15){
+      this.y -= 81;
+    } else if (keyPress === 'down' && this.y < 390){
+      this.y += 81;
+    } else if (keyPress === 'left' && this.x > 0){
+      this.x -= 101;
+    } else if (keyPress === 'right' && this.x < 404){
+      this.x += 101;
+    }
+
+  }
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-let allEnemies = [];
 // Place the player object in a variable called player
-let player = {
-  handleInput: function(e){
+// 66,147,228
 
-  },
-  update: function(){
-
-  },
-  render: function() {
-    
-  }
-};
+const enemy0 = new Enemy (-101,66,300);
+const enemy1 = new Enemy (-101,147,250);
+const enemy2 = new Enemy (-101,228,400);
+const allEnemies = [enemy0,enemy1,enemy2];
+let player = new Player();
 
 
 
